dd95adc1091027861d749e88384608037d8056ce
khsung <khsung0@nate.com>
2020-11-16 15:36:02 +0900

    스택과 큐 수정

--- Ebook_Source_Code/Code_Set.cpp
+++ Ebook_Source_Code/Code_Set.cpp
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #define MAX_STACK_SIZE 100
 
-typedef struct {                //스택 구조체 선언
+typedef struct {                      //스택 구조체 선언
 	int stack[MAX_STACK_SIZE];
 	int top;
 }STACK;
-void init(STACK* s) {    //스택 초기화
+void init(STACK* s) {                //스택 초기화
 	s->top = -1;
 }
-int isempty(STACK* s) {  //스택 공백상태 체크
+int isempty(STACK* s) {              //스택 공백상태 체크
 	if (s->top == -1)
 		return 1;
 	else
 		return 0;
 }
-int isfull(STACK* s) {    //스택 포화상태 체크
-	if (s->top == MAX_STACK_SIZE - 1)
+int isfull(STACK* s) {                //스택 포화상태 체크
+	if (s->top == MAX_STACK_SIZE-1)
 		return 1;
 	else
 		return 0;
 }
-void push(STACK* s, int number) {  //스택 원소 삽입
+void push(STACK* s, int number) {      //스택 원소 삽입
 	if (isfull(s) == 1) {
 		printf("포화상태오류\n");
 	}
@@ -30,7 +30,7 @@ void push(STACK* s, int number) {  //스택 원소 삽입
 		s->stack[s->top] = number;
 	}
 }
-void pop(STACK* s) {    //스택 원소 제거
+void pop(STACK* s) {                   //스택 원소 제거
 	if (isempty(s) == 1) {
 		printf("공백상태오류\n");
 	}
@@ -39,7 +39,7 @@ void pop(STACK* s) {    //스택 원소 제거
 		s->top--;
 	}
 }
-void peek(STACK* s) {    //스택 상단 원소 출력
+void peek(STACK* s) {                //스택 상단 원소 출력
 	if (isempty(s) == 1)
 		printf("공백상태\n");
 	else
@@ -47,7 +47,7 @@ void peek(STACK* s) {    //스택 상단 원소 출력
 		printf("%d\n", s->stack[s->top]);
 	}
 }
-void printstack(STACK* s) {   //스택 출력
+void printstack(STACK* s) {           //스택 출력
 	if (s->top == -1)
 		printf("공백상태\n");
 	else {
@@ -81,28 +81,28 @@ int main() {
 #include <stdio.h>
 #define MAX_QUEUE_SIZE 100
 
-typedef struct {  //큐 구조체 선언
+typedef struct {              //큐 구조체 선언
 	int front;
 	int rear;
 	int queue[MAX_QUEUE_SIZE];
 }QUEUE;
 
-void init(QUEUE* q) {  //큐 초기화
+void init(QUEUE* q) {           //큐 초기화
 	q->front = q->rear = 0;
 }
-int isempty(QUEUE* q) {   //큐 공백상태 체크
+int isempty(QUEUE* q) {       //큐 공백상태 체크
 	if (q->front == q->rear)
 		return 1;
 	else
 		return 0;
 }
-int isfull(QUEUE* q) {  //큐 포화상태 체크
+int isfull(QUEUE* q) {          //큐 포화상태 체크
 	if (q->rear == MAX_QUEUE_SIZE)
 		return 1;
 	else
 		return 0;
 }
-void enqueue(QUEUE* q, int number) { //큐 원소삽입
+void enqueue(QUEUE* q, int number) {    //큐 원소삽입
 	if (isfull(q) == 1)
 		printf("포화 큐\n");
 	else {
@@ -110,7 +110,7 @@ void enqueue(QUEUE* q, int number) { //큐 원소삽입
 		q->rear++;
 	}
 }
-void dequeue(QUEUE* q) {  //큐 원소제거
+void dequeue(QUEUE* q) {       //큐 원소제거
 	if (isempty(q) == 1)
 		printf("공백 큐\n");
 	else {
@@ -118,7 +118,7 @@ void dequeue(QUEUE* q) {  //큐 원소제거
 		q->front++;
 	}
 }
-void printqueue(QUEUE* q) {  //큐 출력
+void printqueue(QUEUE* q) {      //큐 출력
 	if (isempty(q) == 1)
 		printf("공백 큐\n");
 	else {
