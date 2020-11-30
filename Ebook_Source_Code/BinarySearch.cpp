9e320a715b89862d04fb9085570e6b8f80ba7cf0
khsung <khsung0@nate.com>
2020-11-30 12:38:58 +0900

    이진 검색

---
+++ Ebook_Source_Code/BinarySearch.cpp
@@ -0,0 +1,32 @@
+
+#include<stdio.h>
+#include<math.h>
+/*
+int main() {
+	int answer = 5;
+	bool check = false;  //원소가 있는지 체크하는 변수
+	int array[7] = { 1,2,3,4,5,6,7 };
+	int left=0;               //왼쪽 끝 인덱스
+	int right = sizeof(array) / sizeof(int)-1;
+	                                  //오른쪽 끝 인덱스
+	int mid;            //중간 인덱스
+	while (left<=right) {
+		mid = floor((left + right) / 2);
+		if (array[mid] == answer) {
+			printf("정답 원소 : %d, 인덱스 : %d", array[mid], mid);
+			check = true;
+			break;
+		}
+		else if (array[mid] > answer) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+	if (check == false) {
+		printf("해당 원소 없음\n");
+	}
+}
+
+*/
