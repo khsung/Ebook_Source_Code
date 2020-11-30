d6ea8a91a1a4c37da7f8a5ede8a8b1ee68995028
khsung <khsung0@nate.com>
2020-11-30 11:01:09 +0900

    Revert "버블 정렬"
    This reverts commit 7d271238e5bef29049e4fbab235a2d5e786edef7.

--- Ebook_Source_Code/BubbleSort.cpp
+++ Ebook_Source_Code/BubbleSort.cpp
@@ -1,19 +1,13 @@
 /*
 #include<stdio.h>
 
-void printarray(int *array) {     //배열 출력
-	for (int i = 0; i <= sizeof(array); i++) {
-		printf("%d   ", array[i]);
-	}
-	printf("\n");
-}
-
-int main() {
+void main() {
 	int array[5] = { 2,3,1,5,4 };
 	int temp;
 	printf("정렬 전 배열 : ");
-	printarray(array);
-	printf("\n정렬 과정\n");
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+		printf("%d   ", array[i]);
+	}
 	for (int i = sizeof(array) / sizeof(int) - 1; i > 0; i--) {
 		for (int j = 0; j < i; j++) {
 			if (array[j] > array[j + 1]) {  //왼쪽 값이 오른쪽
@@ -21,13 +15,11 @@ int main() {
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
-			printarray(array);
 		}
 	}
 	printf("\n정렬 후 배열 : ");
-	printarray(array);
-	printf("\n");
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+		printf("%d   ", array[i]);
+	}
 }
-
 */
-
