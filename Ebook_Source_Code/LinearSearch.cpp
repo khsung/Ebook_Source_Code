ca275f7fd6b712bf80f5cc66468d4a6f757fe709
khsung <khsung0@nate.com>
2020-11-30 12:17:14 +0900

    선형 검색

---
+++ Ebook_Source_Code/LinearSearch.cpp
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+
+int main() {
+	int answer = 6;
+	bool check = false;  //원소가 있는지 체크하는 변수
+	int array[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+		if (array[i] == answer){
+			printf("검색 완료 : %d, 인덱스 : %d", array[i], i);
+			check = true;
+		}
+	}
+	if (check == false) {
+		printf("해당 원소 없음\n");
+	}
+}
+
