0cc1cc2fda2cb1e59d50568bb7c5726369721b0c
khsung <khsung0@nate.com>
2020-12-01 12:20:53 +0900

    데크

--- Ebook_Source_Code/Deque.cpp
+++ Ebook_Source_Code/Deque.cpp
@@ -1,46 +1,124 @@
-
+/*
 #include <stdio.h>
-#include <malloc.h>
 
-typedef struct {              //노드 구조체 선언
-	int data;
-	struct NODE* prev;
-	struct NODE* next;
-}NODE;
+#define MAX_DEQUE_SIZE 4    //데크 최대 크기
 
 typedef struct {              //데크 구조체 선언
-	NODE* front;
-	NODE* rear;
+	int front, rear;
+	int deque[MAX_DEQUE_SIZE];
 }DEQUE;
 
-void init(DEQUE* d) {           //큐 초기화
-	d->front = d->rear = NULL;
+void init(DEQUE* d) {
+	d->front = 0;
+	d->rear = -1;
 }
 
-int isempty(DEQUE* d) {       //큐 공백상태 체크
-	if (d->front == NULL)
+int isempty(DEQUE* d) {
+	if (d->front > d->rear) {
 		return 1;
-	else
-		return 0;
-}
-
-void enqueuef(DEQUE* d, int data) {
-	NODE* temp = (NODE*)malloc(sizeof(NODE));
-	temp->data = data;
-	if (d->front == NULL) {
-		d->front = temp;
-		d->rear = temp;
-		temp->next = temp->prev = NULL;
 	}
 	else {
-		temp->next = d->front->next;
-		d->front= temp;
-		printf("%d", d->front->next->data);
+		return 0;
+	}
+}
+
+void enqueueF(DEQUE* d, int data) {
+	if (d->front == 0) {
+		if (d->rear == MAX_DEQUE_SIZE - 1) {
+			printf("포화 데크\n");
+		}
+		else {
+			d->rear += 1;
+			if (d->rear==d->front) {
+				d->deque[d->front] = data;
+			}
+			else {
+				for (int i = d->rear; i > d->front; i--) {
+					d->deque[i] = d->deque[i-1];
+				}
+				d->deque[d->front] = data;
+			}
+		}
+	}
+	else {
+		d->front -= 1;
+		d->deque[d->front] = data;
+	}
+}
+
+void enqueueR(DEQUE* d, int data) {
+	if (d->rear == MAX_DEQUE_SIZE-1) {
+		if (d->front == 0) {
+			printf("포화 데크\n");
+		}
+		else {
+			d->front -= 1;
+			if (d->rear == d->front) {
+				d->deque[d->rear] = data;
+			}
+			else {
+				for (int i = d->front; i < d->rear; i++) {
+					d->deque[i] = d->deque[i + 1];
+				}
+				d->deque[d->rear] = data;
+			}
+		}
+	}
+	else {
+		d->rear += 1;
+		d->deque[d->rear] = data;
+	}
+}
+
+void dequeueF(DEQUE* d) {
+	if (isempty(d)) {
+		printf("공백 데크\n");
+	}
+	else {
+		d->front += 1;
+	}
+}
+
+void dequeueR(DEQUE* d) {
+	if (isempty(d)) {
+		printf("공백 데크\n");
+	}
+	else {
+		d->rear -= 1;
+	}
+}
+
+void printdeque(DEQUE* d) {
+	if (d->front > d->rear) {
+		printf("공백 데크\n");
+	}
+	else {
+		printf("데크 : ");
+		for (int i = d->front; i <= d->rear; i++) {
+			printf("%d  ", d->deque[i]);
+		}
+		printf("\n");
 	}
 }
 
 int main() {
-	DEQUE* deque = (DEQUE*)malloc(sizeof(DEQUE));
-	init(deque);
-	enqueuef(deque, 1);
+	DEQUE deque;
+	init(&deque);
+	printdeque(&deque);
+	enqueueF(&deque, 1);
+	enqueueF(&deque, 2);
+	enqueueR(&deque, 3);
+	enqueueR(&deque, 4);
+	enqueueR(&deque, 5);
+	printdeque(&deque);
+	dequeueF(&deque);
+	dequeueR(&deque);
+	printdeque(&deque);
+	enqueueR(&deque, 6);
+	printdeque(&deque);
+	dequeueF(&deque);
+	dequeueF(&deque);
+	dequeueR(&deque);
+	dequeueF(&deque);
 }
+*/
