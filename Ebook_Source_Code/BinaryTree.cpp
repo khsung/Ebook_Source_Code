c9538e8a0de0f89acca6c712b8a72c798dbcb3ba
khsung <khsung0@nate.com>
2020-12-02 16:45:25 +0900

    이진트리 미완성

---
+++ Ebook_Source_Code/BinaryTree.cpp
@@ -0,0 +1,26 @@
+/*
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct TNODE{
+	int data;
+	struct TNODE* right;
+	struct TNODE* left;
+}TNODE;
+
+void inittree(TNODE* t) {
+	t->left = t->left = NULL;
+	t->data = 0;
+}
+
+void insertnode(TNODE* t, int d) {
+	TNODE* temp = (TNODE*)malloc(sizeof(TNODE));
+	temp->left = temp->right = NULL;
+	temp->data = d;
+	if()
+}
+int main() {
+	TNODE* root = (TNODE*)malloc(sizeof(TNODE));
+	inittree(root);
+}
+*/
