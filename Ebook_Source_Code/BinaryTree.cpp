9252b06002d21dffd9e9656378f0d0ada887ad60
khsung <khsung0@nate.com>
2020-12-02 17:35:21 +0900

    이진 트리 미완성

--- Ebook_Source_Code/BinaryTree.cpp
+++ Ebook_Source_Code/BinaryTree.cpp
@@ -1,4 +1,4 @@
-/*
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -8,19 +8,44 @@ typedef struct TNODE{
 	struct TNODE* left;
 }TNODE;
 
-void inittree(TNODE* t) {
+void inittree(TNODE* t, int d) {
 	t->left = t->left = NULL;
-	t->data = 0;
+	t->data = d;
 }
 
 void insertnode(TNODE* t, int d) {
-	TNODE* temp = (TNODE*)malloc(sizeof(TNODE));
-	temp->left = temp->right = NULL;
-	temp->data = d;
-	if()
+	if (t->data > d) {
+		if (t->left == NULL) {
+			TNODE* temp = (TNODE*)malloc(sizeof(TNODE));
+			temp->left = NULL;
+			temp->right = NULL;
+			temp->data = d;
+			t->left = temp;
+			printf("=====");
+		}
+		else {
+			insertnode(t->left, d);
+		}
+	}
+	else {
+		if (t->right == NULL) {
+			TNODE* temp1 = (TNODE*)malloc(sizeof(TNODE));
+			temp1->left = NULL;
+			temp1->right = NULL;
+			temp1->data = d;
+			t->right = temp1;
+		}
+		else {
+			insertnode(t->right, d);
+		}
+	}
 }
 int main() {
 	TNODE* root = (TNODE*)malloc(sizeof(TNODE));
-	inittree(root);
+	inittree(root,3);
+	insertnode(root, 1);
+	insertnode(root, 5);
+	printf("%d   ", root->data);
+	printf("%d   ", root->left->data);
+	printf("%d   ", root->right->data);
 }
-*/
