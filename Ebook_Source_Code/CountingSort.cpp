692371ad72548284decc71966e1b99625d28a3e8
khsung <khsung0@nate.com>
2020-12-03 17:41:11 +0900

    계수 정렬 미완성

---
+++ Ebook_Source_Code/CountingSort.cpp
@@ -0,0 +1,21 @@
+/*
+#include<stdio.h>
+
+int main() {
+	int array[7] = { 4,1,0,4,2,1,2 };
+	int countarray[5] = { 0,0,0,0,0 };
+	printf("정렬 전 배열 : ");
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+		printf("%d  ", array[i]);
+		countarray[array[i]] += 1;
+	}
+
+	//for문으로 정렬 코드 작성하기
+
+	printf("\n정렬 된 배열 : ");
+	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+		printf("%d  ", array[i]);
+	}
+}
+
+*/
