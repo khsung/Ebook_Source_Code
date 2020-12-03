b6675bc278de0902a9c8699540a47b0c502b8a7a
khsung <khsung0@nate.com>
2020-12-03 14:55:48 +0900

    이진트리 실패

--- Ebook_Source_Code/BinaryTree.cpp
+++ Ebook_Source_Code/BinaryTree.cpp
@@ -7,47 +7,23 @@ typedef struct TNODE {
 	struct TNODE* right;
 }TNODE;
 
-void addnode(TNODE** t,int d) {
+typedef struct TREE {
+	TNODE* root;
+}TREE;
+
+
+void addnode(TREE* r,int d) {
 	TNODE* temp = (TNODE*)malloc(sizeof(TNODE));
-	TNODE* curr=*t;
-	temp->data = d;
-	temp->left = NULL;
-	temp->right = NULL;
-	if (curr == NULL) {
-		(*curr) = *temp;
-		printf("루트 완료\n");
-	}
-	else {
-		while (true) {
-			if (curr->data > temp->data) {
-				if (curr->left == NULL) {
-					curr->left = temp;
-					printf("왼쪽자식 삽입\n");
-					break;
-				}
-				else {
-					curr = curr->left;
-				}
-			}
-			else {
-				if (curr->right == NULL) {
-					curr->right = temp;
-					printf("오른쪽자식 삽입\n");
-					break;
-				}
-				else {
-					curr = curr->right;
-				}
-			}
-		}
+	if (r->root == NULL) {
+		r->root = temp;
+		temp->left = NULL;
+		temp->right = NULL;
+		temp->data = d;
 	}
 }
 
 int main() {
-	TNODE* root= NULL;
-	addnode(&root, 3);
-	addnode(&root, 1);
-	addnode(&root, 5);
-	//printf("%d", root->data);
-	//printf("%s", root->left);
+	TREE* root;
+	root->root = NULL;
+
 }
