254de1543e316d07fdf1a7fd0c449c8998b1ee18
khsung <khsung0@nate.com>
2020-12-07 14:22:27 +0900

    힙

--- Ebook_Source_Code/Heap.cpp
+++ Ebook_Source_Code/Heap.cpp
@@ -1,18 +1,18 @@
-
+/*
 #include<stdio.h>
-#define MAX_HEAP_SIZE 100
+#define MAX_HEAP_SIZE 100  //힙 최대 크기
 
-typedef struct {
+typedef struct {        //최대 힙 구조체 정의
 	int currheapsize;
 	int heap[MAX_HEAP_SIZE];
 }MAXHEAP;
 
-void init(MAXHEAP* h) {
+void init(MAXHEAP* h) {   //힙 초기화
 	h->currheapsize = 0;
 }
 
-int isempty(MAXHEAP* h) {
-	if (h->currheapsize == 0) {
+int isempty(MAXHEAP* h) {   //공백 상태일 때 true 반환
+	if (h->currheapsize == 0) {  
 		return 1;
 	}
 	else {
@@ -20,7 +20,7 @@ int isempty(MAXHEAP* h) {
 	}
 }
 
-int isfull(MAXHEAP* h) {
+int isfull(MAXHEAP* h) {      //포화 상태일 때 true 반환
 	if (h->currheapsize == MAX_HEAP_SIZE-1) {
 		return 1;
 	}
@@ -29,21 +29,23 @@ int isfull(MAXHEAP* h) {
 	}
 }
 
-void swap(int* a, int* b) {
+void swap(int* a, int* b) {     //값 교환
 	int temp;
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void addheap(MAXHEAP* h, int data) {
+void addheap(MAXHEAP* h, int data) {     //원소 값 추가
 	if (isfull(h)) {
 		printf("포화 힙\n");
 	}
 	else {
 		h->currheapsize++;
 		int currindex = h->currheapsize;
-		h->heap[currindex] = data;
+		h->heap[currindex] = data;    //마지막 인덱스에 값 추가
+		
+		//최대 힙 정렬 과정, 부모 값보다 자식 값이 크면 서로 교환
 		while (currindex != 1) {
 			if (h->heap[currindex] > h->heap[currindex / 2]) {
 				swap(&h->heap[currindex], &h->heap[currindex / 2]);
@@ -56,20 +58,20 @@ void addheap(MAXHEAP* h, int data) {
 	}
 }
 
-void deleteheap(MAXHEAP* h) {
+void deleteheap(MAXHEAP* h) {      //최대 값 원소 제거
 	if (isempty(h)) {
 		printf("공백 힙\n");
 	}
 	else {
-		if (h->currheapsize == 1) {
-			h->currheapsize--;
+		if (h->currheapsize == 1) {    //원소개수가 한 개일 때
+			h->currheapsize--;         //공백상태로 전환
 		}
 		else {
 			int curr = 1;
 			h->heap[curr] = h->heap[h->currheapsize];
 			h->currheapsize--;
 
-			//현재 힙의 크기까지만 반복가능
+			//현재 힙의 크기까지만 탐색하도록 제한
 			while (2 * curr + 1 <= h->currheapsize) {
 
 				//왼쪽 자식만 있을 때
@@ -82,6 +84,8 @@ void deleteheap(MAXHEAP* h) {
 						break;
 					}
 				}
+
+				//왼쪽 오른쪽 둘 다 있을 때
 				else {
 
 					//오른쪽 자식이 클 때
@@ -112,12 +116,17 @@ void deleteheap(MAXHEAP* h) {
 }
 
 void printheap(MAXHEAP* h) {
-	int line = 2;
-	for (int i = 1; i <= h->currheapsize; i++) {
-		printf("%d  ", h->heap[i]);
-		if (i == line - 1) {
-			printf("\n");
-			line = 2 * line;
+	if (h->currheapsize == 0) {
+		printf("공백 힙\n");
+	}
+	else {
+		int line = 2;
+		for (int i = 1; i <= h->currheapsize; i++) {
+			printf("%d  ", h->heap[i]);
+			if (i == line - 1) {
+				printf("\n");
+				line = 2 * line;
+			}
 		}
 	}
 	printf("\n");
@@ -126,17 +135,16 @@ void printheap(MAXHEAP* h) {
 int main() {
 	MAXHEAP heap;
 	init(&heap);
-	for (int i = 0; i < 10; i++) {
-		addheap(&heap, i);
-	}
-	//printheap(&heap);
-	/*addheap(&heap, 5);
-	addheap(&heap, 4);
+	printheap(&heap);
+	addheap(&heap, 6);
+	addheap(&heap, 3);
+	addheap(&heap, 5);
+	addheap(&heap, 7);
+	addheap(&heap, 9);
+	addheap(&heap, 1);
 	addheap(&heap, 6);
 	printheap(&heap);
-	addheap(&heap, 8);
-	addheap(&heap, 10);
-	addheap(&heap, 9);*/
-	//addheap(&heap, 4);
+	deleteheap(&heap);
 	printheap(&heap);
 }
+*/
