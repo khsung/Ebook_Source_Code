3cb0635856cb3fc93e8f6317952180ef6304de25
khsung <khsung0@nate.com>
2020-12-10 17:06:33 +0900

    재귀 알고리즘

---
+++ Ebook_Source_Code/RecursiveAlgorithm.cpp
@@ -0,0 +1,18 @@
+/*
+#include<stdio.h>
+
+void recursive(int num) {
+	if (num == 0) {     //종료 조건
+		printf("count 종료");
+	}
+	else {
+		printf("count : %d\n",num);
+		recursive(num-1);
+	}
+}
+
+int main() {
+	int num = 5;
+	recursive(num);
+}
+*/
