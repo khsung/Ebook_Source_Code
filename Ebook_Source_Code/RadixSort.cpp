a7e57791e6bcd190fb122029e45dac1bae894a3b
khsung <khsung0@nate.com>
2020-12-14 09:44:11 +0900

    기수 정렬

--- Ebook_Source_Code/RadixSort.cpp
+++ Ebook_Source_Code/RadixSort.cpp
@@ -18,6 +18,7 @@ void initqueue(QUEUE* q) {           //큐 초기화
 	}
 }
 
+//10개의 큐에 연산값에 따른 데이터 추가
 void adddata(QUEUE* q, int index, int data) {
 	q->rear[index]++;
 	q->queue[index][q->rear[index]] = data;
@@ -39,14 +40,25 @@ int findmaxdigit(int array[],int size) {
 	return maxdigit;
 }
 
-void resortToOriginArray(QUEUE* q, int array[], int size) {
+//큐에서 데이터를 원래 배열로 분배
+void distributeToOriginArray(QUEUE* q, int array[], int size) {
+	//queueindex : 큐 구조체의 인덱스(0~9)
+	//arrayindex : 원래 배열의 인덱스
 	int queueindex = 0, arrayindex = 0;
-	while (queueindex < size) {
-		while (q->front[arrayindex] <= q->rear[arrayindex]) {
-
+	for (int i = 0; i < 10; i++) {
+		while (q->front[i] <= q->rear[i]) {
+			array[arrayindex] = q->queue[i][q->front[i]];
+			q->front[i]++;
+			arrayindex++;
 		}
-		arrayindex++;
 	}
+}
+
+void printarray(int array[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d  ", array[i]);
+	}
+	printf("\n");
 }
 
 int main() {
@@ -56,13 +68,22 @@ int main() {
 	int currdigit = 1;
 	QUEUE queue;
 	initqueue(&queue);
+	printf("정렬 전 배열 : ");
+	printarray(array, arraysize);
+	printf("\n정렬 과정\n");
 	for (int i = 1; i <= maxdigit; i++) {
 		for (int j = 0; j < arraysize; j++) {
+			//(array[j]/currdigit)%10 이란 
+			//i==1인 첫 번째 반복문은 1의자리, 두 번째는 10의자리,
+			//n번째 반복문은 10^(n-1)의 자리를 뜻한다.
 			adddata(&queue, (array[j] / currdigit) % 10, array[j]);
 		}
-		//초기화 시키기전에 원래 배열에 분배
+		distributeToOriginArray(&queue, array, arraysize);
+		initqueue(&queue);
 		currdigit *= 10;
+		printarray(array, arraysize);
 	}
-
+	printf("\n정렬 된 배열 : ");
+	printarray(array, arraysize);
 }
 */
