e2333ba0490c6dfd7bb76a25f390d0ccb442bfad
khsung <khsung0@nate.com>
2020-12-21 11:58:51 +0900

    백트래킹 미완성

---
+++ Ebook_Source_Code/Backtracking(N_Queen).cpp
@@ -0,0 +1,15 @@
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main() {
+//	int num;
+//	scanf_s("%d", &num);
+//	int* queen = (int*)calloc(num, sizeof(int)*num);
+//	int* chess = (int*)calloc(num, sizeof(int) * num);
+//	printf("%d", queen[0][0]);
+//
+//	free(queen);
+//	free(chess);
+//}
