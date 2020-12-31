24085fd1ebf90f676d53218c02ec07572f4a8833
khsung <khsung0@nate.com>
2020-12-31 15:13:18 +0900

    다익스트라 알고리즘

--- Ebook_Source_Code/DijkstraAlgorithm.cpp
+++ Ebook_Source_Code/DijkstraAlgorithm.cpp
@@ -1,9 +1,10 @@
-
+/*
 #include<stdio.h>
 
 #define GRAPH_SIZE 5    //그래프 크기 선언
-#define INF 999         //무한대 대체 변수선언
+#define INF 999         //무한대 대체 변수선언 상황에 따라 변경
 
+//배열 출력 함수
 void printarray(int array[], int arraysize) {
 	for (int i = 0; i < arraysize; i++) {
 		printf("%d ", array[i]);
@@ -12,15 +13,19 @@ void printarray(int array[], int arraysize) {
 }
 
 int main() {
-	int graph[GRAPH_SIZE][GRAPH_SIZE] = { {0,2,8,5,INF},{2,0,INF,1,9},{8,INF,0,2,INF},{5,1,2,0,4},{INF,9,INF,4,0} };
+	//그래프 행렬 선언
+	int graph[GRAPH_SIZE][GRAPH_SIZE] = { {0,2,8,5,INF},{2,0,INF,1,9},{8,INF,0,2,INF},
+										{5,1,2,0,4},{INF,9,INF,4,0} };
+	//경로 저장 배열 선언
 	int path[GRAPH_SIZE] = { 0 };
+	//비용 저장 배열 선언
 	int cost[GRAPH_SIZE] = { INF, INF, INF, INF, INF };
+
 	//방문했는지 체크하는 배열, 배열크기만큼 false로 자동 초기화
 	bool visited[GRAPH_SIZE] = {};
-	int min, minindex;
-	int startnode = 0;
-
-	cost[startnode] = 0;
+	int min, minindex;            //최소값과 최소값의 인덱스 저장 변수
+	int startnode = 0;            //시작 노드
+	cost[startnode] = 0;          //시작 노드 비용 갱신
 	
 	//그래프 출력
 	printf("그래프 행렬\n");
@@ -34,19 +39,24 @@ int main() {
 	printf("\n초기 비용 배열 : ");
 	printarray(cost, GRAPH_SIZE);
 
+	//노드 개수만큼 반복
 	for (int i = 0; i < GRAPH_SIZE; i++) {
+		//방문한 노드로 변경
 		visited[startnode] = true;
 		printf("\n시작 노드 : %d\n", startnode);
 		for (int j = 0; j < GRAPH_SIZE; j++) {
-			if (visited[j] == false && (cost[startnode] + graph[startnode][j]) < cost[j]) {
+			//방문하지 않은 노드 중 비용이 더 적을 경우 비용 배열 갱신
+			if ((visited[j]==false)&&((cost[startnode]+graph[startnode][j])<cost[j])){
 				cost[j] = cost[startnode] + graph[startnode][j];
 				path[j] = startnode;
 			}
 		}
-		printf("갱신된 비용 : ");
+
+		printf("갱신된 비용 배열 : ");
 		printarray(cost, GRAPH_SIZE);
 		min = INF;
 
+		//방문하지 않은 노드 중 최소값과 최소값의 인덱스 구하는 반복문
 		for (int j = 0; j < GRAPH_SIZE; j++) {
 			if (visited[j] == false && cost[j] < min) {
 				min = cost[j];
@@ -54,6 +64,7 @@ int main() {
 			}
 		}
 
+		//min이 무한대일 경우는 모든 노드를 방문했단 뜻
 		if (min != INF) {
 			startnode = minindex;
 		}
@@ -66,12 +77,14 @@ int main() {
 	for (int i = 0; i < GRAPH_SIZE; i++) {
 		printf("목적지 노드 : %d  최소비용 경로 : ", i);
 		printf("%d", i);
+		//현재노드 임시저장
 		int temp = i;
 		while (temp != 0) {
+			//경로 배열의 값은 다음 인덱스를 의미
 			temp = path[temp];
 			printf(" <- %d", temp);
 		}
-		printf("\n");
+		printf("\n\n");
 	}
-
 }
+*/
