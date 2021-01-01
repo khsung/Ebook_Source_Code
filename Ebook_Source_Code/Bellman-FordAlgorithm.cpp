3b9bd9a475f9b18c1bbb1f228707530d523ec49d
khsung <khsung0@nate.com>
2021-01-01 18:20:09 +0900

    벨만 포드 알고리즘

--- Ebook_Source_Code/Bellman-FordAlgorithm.cpp
+++ Ebook_Source_Code/Bellman-FordAlgorithm.cpp
@@ -1,15 +1,17 @@
 
-//#include<stdio.h>
-//#define INF 999
-//#define GRAPH_SIZE 5
-//
-//
-//void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]);
-//
-//int main() {
-//
-//}
-//
-//void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]) {
-//
-//}
+#include<stdio.h>
+#define INF 999
+#define GRAPH_SIZE 5
+#define STARTNODE 0
+
+void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]);
+
+int main() {
+	int startnode = STARTNODE;
+	int graph[GRAPH_SIZE][GRAPH_SIZE];
+
+}
+
+void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]) {
+
+}
