d0a8f5a695b46834fa55bbb326b4267cd73d8ff1
khsung <khsung0@nate.com>
2021-01-02 00:43:51 +0900

    벨만포드 미완성

--- Ebook_Source_Code/Bellman-FordAlgorithm.cpp
+++ Ebook_Source_Code/Bellman-FordAlgorithm.cpp
@@ -1,17 +1,26 @@
 
-#include<stdio.h>
-#define INF 999
-#define GRAPH_SIZE 5
-#define STARTNODE 0
-
-void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]);
-
-int main() {
-	int startnode = STARTNODE;
-	int graph[GRAPH_SIZE][GRAPH_SIZE];
-
-}
-
-void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]) {
-
-}
+//#include<stdio.h>
+//#define INF 999
+//#define GRAPH_SIZE 5
+//#define STARTNODE 0
+//
+//void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]);
+//
+//int main() {
+//	int startnode = STARTNODE;
+//	int graph[GRAPH_SIZE][GRAPH_SIZE] = { {0,6,4,9,INF},{-3,0,INF,1,6},
+//					{-3,INF,0,2,INF},{INF,INF,INF,0,4},{INF,2,INF,INF,0} };
+//	int cost[GRAPH_SIZE] = { INF ,INF ,INF ,INF ,INF };
+//	cost[startnode] = 0;
+//
+//	for (int i = 0; i < GRAPH_SIZE; i++) {
+//		for (int j = 0; j < GRAPH_SIZE; j++) {
+//			printf("%d  ", graph[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//
+//void bellmanford(int graph[GRAPH_SIZE][GRAPH_SIZE]) {
+//
+//}
