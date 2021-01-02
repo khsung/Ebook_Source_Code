58be422373328b0f810605083d6e729be1bd292e
khsung <khsung0@nate.com>
2021-01-02 19:37:06 +0900

    세그먼트트리

--- Ebook_Source_Code/SegmentTree.cpp
+++ Ebook_Source_Code/SegmentTree.cpp
@@ -1,6 +1,16 @@
 
 #include<stdio.h>
+#define ARRAY_SIZE 7
+#define SEGMENT_SIZE ARRAY_SIZE*4
+
+int initSegment(int origin[], int segment[],int left, int right, int mid) {
+
+	return 0;
+}
 
 int main() {
+	int originarray[ARRAY_SIZE] = { 4,7,1,-3,10,-1,6 };
+	int segmenttree[SEGMENT_SIZE];
+
 
 }
