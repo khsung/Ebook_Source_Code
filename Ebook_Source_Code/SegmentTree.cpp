da718d840cd4e948be1535d1d164c6589c302d47
khsung <khsung0@nate.com>
2021-01-02 22:13:07 +0900

    세그먼트 트리 미완성

--- Ebook_Source_Code/SegmentTree.cpp
+++ Ebook_Source_Code/SegmentTree.cpp
@@ -1,9 +1,9 @@
 
 #include<stdio.h>
-#define ARRAY_SIZE 7
-#define SEGMENT_SIZE ARRAY_SIZE*4
+#define ARRAY_SIZE 7                 //배열 크기 선언
+#define SEGMENT_SIZE ARRAY_SIZE*4    //편의상 배열크기의 4배를 선언
 
-int initSegment(int origin[], int segment[],int left, int right, int mid) {
+int initSegment(int origin[], int segment[],int left, int right, int mid, int index, int maxindex) {
 
 	return 0;
 }
@@ -11,6 +11,7 @@ int initSegment(int origin[], int segment[],int left, int right, int mid) {
 int main() {
 	int originarray[ARRAY_SIZE] = { 4,7,1,-3,10,-1,6 };
 	int segmenttree[SEGMENT_SIZE];
-
+	int segmentIndex = 1;       //세그먼트 트리의 루트 인덱스(모든 원소의 합) 선언
+	int segmentMaxIndex = 0;    //세그먼트 트리의 최대 인덱스를 구하는 변수 선언
 
 }
