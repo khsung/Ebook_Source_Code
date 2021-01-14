66bfa9c10b3864eb41a20b1ce73b7629c7cfa4da
khsung <khsung0@nate.com>
2021-01-14 12:10:21 +0900

    스택

--- Ebook_Source_Code/Stack.cpp
+++ Ebook_Source_Code/Stack.cpp
@@ -1,11 +1,10 @@
 /*
 #include <stdio.h>
- 
-#define MAX_STACK_SIZE 100
+#define MAX_STACK_SIZE 100			//스택 최대 크기 선언
 
-typedef struct {                      //스택 구조체 선언
-	int stack[MAX_STACK_SIZE];
-	int top;
+typedef struct {                    //스택 구조체 선언
+	int stack[MAX_STACK_SIZE];		//스택 배열
+	int top;						//스택 최상단 인덱스
 }STACK;
 
 void init(STACK* s) {                //스택 초기화
@@ -25,7 +24,7 @@ int isfull(STACK* s) {                //스택 포화상태 체크
 }
 void push(STACK* s, int number) {      //스택 원소 삽입
 	if (isfull(s) == 1) {
-		printf("포화상태오류\n");
+		printf("포화 스택\n");
 	}
 	else {
 		s->top++;
@@ -34,7 +33,7 @@ void push(STACK* s, int number) {      //스택 원소 삽입
 }
 void pop(STACK* s) {                   //스택 원소 제거
 	if (isempty(s) == 1) {
-		printf("공백상태오류\n");
+		printf("공백 스택\n");
 	}
 	else {
 		s->top--;
@@ -42,15 +41,15 @@ void pop(STACK* s) {                   //스택 원소 제거
 }
 void peek(STACK* s) {                //스택 상단 원소 출력
 	if (isempty(s) == 1)
-		printf("공백상태\n");
+		printf("공백 스택\n");
 	else
 	{
-		printf("상단원소 : %d\n", s->stack[s->top]);
+		printf("상단 원소 : %d\n", s->stack[s->top]);
 	}
 }
 void printstack(STACK* s) {           //스택 출력
 	if (s->top == -1)
-		printf("공백상태\n");
+		printf("공백 스택\n");
 	else {
 		printf("스택 : ");
 		for (int i = 0; i <= s->top; i++) {
@@ -71,5 +70,4 @@ int main() {
 	pop(&teststack);
 	printstack(&teststack);
 }
-
 */
