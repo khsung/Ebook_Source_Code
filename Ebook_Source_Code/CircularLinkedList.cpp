ee95c19096875515c273371320cf12c20b653185
khsung <khsung0@nate.com>
2021-01-14 11:15:33 +0900

    원형 연결 리스트

--- Ebook_Source_Code/CircularLinkedList.cpp
+++ Ebook_Source_Code/CircularLinkedList.cpp
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <malloc.h>
 
-typedef struct NODE {     //노드 구조체 선언
-	int data;
-	struct NODE* next;
+typedef struct NODE {		//노드 구조체 선언
+	int data;				//노드의 키 값
+	struct NODE* next;		//다음 노드
 }NODE;
 
-void addnode(NODE* n, int d) {     //뒤에 노드 추가
+void addnode(NODE* n, int d) {     //끝에 노드 추가
+	//추가할 노드 초기화
 	NODE* temp = (NODE*)malloc(sizeof(NODE));
 	NODE* curr = n;
 	temp->data = d;
 	temp->next = NULL;
+	//공백 노드일 때 노드를 추가하고 자기 자신을 가리킴
 	if (n->next == NULL) {
 		n->next = temp;
 		temp->next = n->next;
 	}
 	else {
+		//원형이기 때문에 먼저 다음 노드로 이동하고
+		//헤드 노드의 다음 노드를 가리키기 전까지 반복
 		do {
 			curr = curr->next;
 		} while (curr->next != n->next);
@@ -36,14 +40,19 @@ void deletenode(NODE* n, int d) {     //원하는 노드 삭제
 		do {
 			if (curr->next->data == d) {
 				delnode = curr->next;
+				//노드가 자기 자신을 가리킬 때
+				//즉 헤드 노드를 제외한 노드가 한개일 때
 				if (delnode->next == delnode) {
 					n->next = NULL;
 					free(delnode);
 					return;
 				}
 				else {
+					//지울 노드가 헤드 노드 다음 노드일 때
 					if (delnode == n->next) {
 						temp = n;
+						//끝 노드의 다음 노드를
+						//지울 노드의 다음 노드로 연결
 						do {
 							temp = temp->next;
 						} while (temp->next != n->next);
@@ -81,6 +90,7 @@ void printnode(NODE* n) {     //원형 리스트 출력
 }
 
 int main(void) {
+	//헤드 노드 초기화
 	NODE* head = (NODE*)malloc(sizeof(NODE));
 	head->data = NULL;
 	head->next = NULL;
