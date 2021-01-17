f2958d2095f2a29ed6a89cb0aed062f382c183b6
khsung <khsung0@nate.com>
2021-01-17 22:48:42 +0900

    삽입 정렬

--- Ebook_Source_Code/InsertionSort.cpp
+++ Ebook_Source_Code/InsertionSort.cpp
@@ -1,5 +1,6 @@
 /*
 #include<stdio.h>
+#define ARRAY_SIZE 5						//배열 크기 선언
 
 void printarray(int* array, int size) {     //배열 출력
 	for (int i = 0; i < size; i++) {
@@ -9,24 +10,25 @@ void printarray(int* array, int size) {     //배열 출력
 }
 
 int main() {
-	int array[5] = { 2,3,1,5,4 };
-	int tempindex,temp,arraysize = sizeof(array)/sizeof(int);
+	int array[ARRAY_SIZE] = { 2, 3, 1, 5, 4 };
+	int tempindex, temp;
 	printf("정렬 전 배열 : ");
-	printarray(array, arraysize);
-	printf("\n\n정렬 과정\n");
-	for (int i = 1; i <sizeof(array)/sizeof(int); i++) {
+	printarray(array, ARRAY_SIZE);
+
+	printf("\n\n삽입 정렬 과정\n");
+	//정렬할 인덱스 반복, 0번째 인덱스는 정렬되어 있다고 가정
+	for (int i = 1; i < ARRAY_SIZE; i++) {
 		tempindex = i;
+		//현재 원소가 앞의 원소보다 클 때까지 반복하여 교환 
 		while (tempindex >= 1 && (array[tempindex] < array[tempindex - 1])) {
 			temp = array[tempindex];
 			array[tempindex] = array[tempindex - 1];
 			array[tempindex - 1] = temp;
 			tempindex--;
 		}
-		printarray(array, arraysize);
-
+		printarray(array, ARRAY_SIZE);
 	}
 	printf("\n정렬 후 배열 : ");
-	printarray(array, arraysize);
+	printarray(array, ARRAY_SIZE);
 }
-
 */
