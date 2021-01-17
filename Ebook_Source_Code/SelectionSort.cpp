ba2c41430d0ef8d76f61423f91a105eb46c07d76
khsung <khsung0@nate.com>
2021-01-17 18:29:41 +0900

    선택 정렬

--- Ebook_Source_Code/SelectionSort.cpp
+++ Ebook_Source_Code/SelectionSort.cpp
@@ -1,6 +1,8 @@
 /*
 #include<stdio.h>
+#define ARRAY_SIZE 5
 
+//배열 출력 함수
 void printarray(int array[],int size) {
 	for (int i = 0; i < size; i++) {
 		printf("%d  ", array[i]);
@@ -9,26 +11,30 @@ void printarray(int array[],int size) {
 }
 
 int main() {
-	int array[5] = { 2,3,1,5,4 };
-	int temp, min, arraysize = sizeof(array)/sizeof(int);
+	int array[ARRAY_SIZE] = { 2,3,1,5,4 };
+	int temp, min;
 	printf("정렬 전 배열 : ");
-	printarray(array, arraysize);
+	printarray(array, ARRAY_SIZE);
+
 	printf("정렬 과정\n");
-	for (int i = 0; i < sizeof(array) / sizeof(int); i++) {
+	for (int i = 0; i < ARRAY_SIZE; i++) {
 		min = i;
-		for (int j = i; j < sizeof(array) / sizeof(int); j++) {
-			if (array[min] > array[j]) {    //최솟값의 인덱스 저장
+		for (int j = i; j < ARRAY_SIZE; j++){
+			//최솟값의 인덱스 저장
+			if (array[min] > array[j]) {
 				min = j;
 			}
 		}
-		if (min != i) {            //정렬될 인덱스 값과 최솟값이
-			temp = array[i];       //다를 경우에만 교환
+		//정렬될 인덱스 값과 최솟값이 다를 경우에만 교환
+		if (min != i) {
+			temp = array[i];
 			array[i] = array[min];
 			array[min] = temp;
 		}
-		printarray(array, arraysize);
+		printarray(array, ARRAY_SIZE);
 	}
 	printf("\n정렬 후 배열 : ");
-	printarray(array, arraysize);
+	printarray(array, ARRAY_SIZE);
 }
+
 */
