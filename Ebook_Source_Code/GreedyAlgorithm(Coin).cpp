1f210b5e81581c89b5bb6a31f0561fd1f9b511d6
khsung <khsung0@nate.com>
2021-01-18 16:02:27 +0900

    탐욕 알고리즘

--- Ebook_Source_Code/GreedyAlgorithm(Coin).cpp
+++ Ebook_Source_Code/GreedyAlgorithm(Coin).cpp
@@ -1,18 +1,20 @@
 
 /*
 #include<stdio.h>
+#define COIN_NUM 4
 
 int main() {
-	int coinValue[4] = { 500,100,50,10 };  //동전 금액
-	int coinCount[4] = { 0 };              //지불할 동전 개수
-	int cost;                              //지불해야 되는 비용
+	//동전 금액
+	int coinValue[COIN_NUM] = { 500, 100, 50, 10 };
+	int coinCount[COIN_NUM] = { 0 };			//지불할 동전 개수
+	int cost;									//지불해야 되는 비용
 
 	printf("지불 금액 : ");
 	scanf_s("%d", &cost);
 
-	for (int i = 0; i<int(sizeof(coinValue) / sizeof(int)); i++) {
+	for (int i = 0; i< COIN_NUM; i++) {
 		//최대한 적은 개수를 내기 위해 가장 큰 금액의 동전부터 처리
-		//이게 그리디 알고리즘의 핵심
+		//그리디 알고리즘의 핵심원리
 		while (coinValue[i] <= cost) {
 			cost -= coinValue[i];
 			coinCount[i]++;
@@ -20,8 +22,9 @@ int main() {
 	}
 
 	//각 동전의 개수 출력
-	for (int i = 0; i<int(sizeof(coinValue) / sizeof(int)); i++) {
+	for (int i = 0; i< COIN_NUM; i++) {
 		printf("%d원 동전의 개수 : %d\n", coinValue[i], coinCount[i]);
 	}
 }
+
 */
