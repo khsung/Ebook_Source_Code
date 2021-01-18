8ed6804705497d6cded0644608f77d9f5a9c0a85
khsung <khsung0@nate.com>
2021-01-18 11:38:20 +0900

    기수 정렬

--- Ebook_Source_Code/RadixSort.cpp
+++ Ebook_Source_Code/RadixSort.cpp
@@ -1,17 +1,15 @@
 /*
 #include<stdio.h>
-
-//배열의 원소 개수
-#define MAX_QUEUE_SIZE 9
+#define ARRAY_SIZE 9					//배열 원소 개수
 
 typedef struct QUEUE {
-	//[0부터9까지의 인덱스][원소의 개수]
-	int queue[10][MAX_QUEUE_SIZE];
-	int front[10] = { 0 }; 
-	int rear[10] = { -1,-1,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 ,-1 };
+	//[0부터 9까지의 인덱스][원소의 개수]
+	int queue[10][ARRAY_SIZE];
+	int front[10];
+	int rear[10];
 }QUEUE;
 
-void initqueue(QUEUE* q) {           //큐 초기화
+void initqueue(QUEUE* q) {				//큐 초기화
 	for (int i = 0; i < 10; i++) {
 		q->front[i] = 0;
 		q->rear[i] = -1;
@@ -25,11 +23,12 @@ void adddata(QUEUE* q, int index, int data) {
 }
 
 //원소들의 최대 자리수 찾는 함수
-int findmaxdigit(int array[],int size) {
-	int maxdigit=0, count, temp;
+int findmaxdigit(int array[], int size) {
+	int maxdigit = 0, count, temp;
 	for (int i = 0; i < size; i++) {
 		count = 0;
 		temp = array[i];
+		//자리수 만큼 count 증가
 		while (temp != 0) {
 			count++;
 			temp /= 10;
@@ -54,6 +53,7 @@ void distributeToOriginArray(QUEUE* q, int array[], int size) {
 	}
 }
 
+//배열 출력
 void printarray(int array[], int size) {
 	for (int i = 0; i < size; i++) {
 		printf("%d  ", array[i]);
@@ -63,27 +63,26 @@ void printarray(int array[], int size) {
 
 int main() {
 	int array[9] = { 541,303,9,70,5,47,11,3,155 };
-	int arraysize = sizeof(array) / sizeof(int);
-	int maxdigit = findmaxdigit(array, arraysize);
+	int maxdigit = findmaxdigit(array, ARRAY_SIZE);
 	int currdigit = 1;
 	QUEUE queue;
 	initqueue(&queue);
 	printf("정렬 전 배열 : ");
-	printarray(array, arraysize);
-	printf("\n정렬 과정\n");
+	printarray(array, ARRAY_SIZE);
+	printf("\n기수 정렬 과정\n");
 	for (int i = 1; i <= maxdigit; i++) {
-		for (int j = 0; j < arraysize; j++) {
+		for (int j = 0; j < ARRAY_SIZE; j++) {
 			//(array[j]/currdigit)%10 이란 
 			//i==1인 첫 번째 반복문은 1의자리, 두 번째는 10의자리,
 			//n번째 반복문은 10^(n-1)의 자리를 뜻한다.
 			adddata(&queue, (array[j] / currdigit) % 10, array[j]);
 		}
-		distributeToOriginArray(&queue, array, arraysize);
+		distributeToOriginArray(&queue, array, ARRAY_SIZE);
 		initqueue(&queue);
 		currdigit *= 10;
-		printarray(array, arraysize);
+		printarray(array, ARRAY_SIZE);
 	}
 	printf("\n정렬 된 배열 : ");
-	printarray(array, arraysize);
+	printarray(array, ARRAY_SIZE);
 }
 */
