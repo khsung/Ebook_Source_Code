a9da4bba6afdc9c1ae67e3e862dad41a3fa12514
khsung <khsung0@nate.com>
2021-01-22 16:34:06 +0900

    재귀 알고리즘

--- Ebook_Source_Code/RecursiveAlgorithm.cpp
+++ Ebook_Source_Code/RecursiveAlgorithm.cpp
@@ -4,17 +4,17 @@
 int recursive(int num) {
 	//종료 조건
 	if (num == 0) {
-		printf("count 종료\n");
+		printf("sum 종료\n");
 		return num;
 	}
 	else {
-		printf("count : %d\n",num);
+		printf("num : %d\n",num);
 		return num + recursive(num - 1);
 	}
 }
 
 int main() {
 	int num = 5;
-	printf("총 걸린 시간 : %d",recursive(num));
+	printf("총 합 : %d",recursive(num));
 }
 */
