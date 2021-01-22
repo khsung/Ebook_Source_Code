307769703d0de45d747c6b0cde9f8b0cab850bce
khsung <khsung0@nate.com>
2021-01-22 10:49:10 +0900

    선택 정렬

--- Ebook_Source_Code/SelectionSort.cpp
+++ Ebook_Source_Code/SelectionSort.cpp
@@ -1,6 +1,6 @@
 /*
 #include<stdio.h>
-#define ARRAY_SIZE 5
+#define ARRAY_SIZE 5		//배열 크기 선언
 
 //배열 출력 함수
 void printarray(int array[],int size) {
@@ -12,24 +12,24 @@ void printarray(int array[],int size) {
 
 int main() {
 	int array[ARRAY_SIZE] = { 2,3,1,5,4 };
-	int temp, min;
+	int temp, minindex;
 	printf("정렬 전 배열 : ");
 	printarray(array, ARRAY_SIZE);
 
 	printf("\n선택 정렬 과정\n");
 	for (int i = 0; i < ARRAY_SIZE; i++) {
-		min = i;
+		minindex = i;
 		for (int j = i; j < ARRAY_SIZE; j++){
 			//최솟값의 인덱스 저장
-			if (array[min] > array[j]) {
-				min = j;
+			if (array[minindex] > array[j]) {
+				minindex = j;
 			}
 		}
 		//정렬될 인덱스 값과 최솟값이 다를 경우에만 교환
-		if (min != i) {
+		if (minindex != i) {
 			temp = array[i];
-			array[i] = array[min];
-			array[min] = temp;
+			array[i] = array[minindex];
+			array[minindex] = temp;
 		}
 		printarray(array, ARRAY_SIZE);
 	}
